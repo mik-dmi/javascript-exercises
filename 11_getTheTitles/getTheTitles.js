7dccb927b3073a9c0ddbba077714f8c4f4fec92e
mik-dmi <122227240+mik-dmi@users.noreply.github.com>
2023-07-18 03:40:09 +0100

    All the exercises are solved

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr){
+    let books = [];
+    for(let i = 0; i < arr.length; i++){
+        books[i] = arr[i].title 
+    }
+    console.log(books);
+    return books;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let oldest = arr[0];
+    let ageCurr = null;
+    let ageAccum = null;
+    return arr.reduce((accumulator, currentValue) =>{ 
+        const currentYear = new Date().getFullYear();
+        if(!currentValue.yearOfDeath) ageCurr =  currentYear   - currentValue.yearOfBirth;
+        else{ 
+            ageCurr =  currentValue.yearOfDeath - currentValue.yearOfBirth }
+        
+            if( !accumulator.yearOfDeath)  ageAccum =  currentYear   - accumulator.yearOfBirth;
+        else{ 
+            ageAccum = accumulator.yearOfDeath -accumulator.yearOfBirth   }
+        return ageAccum > ageCurr ? accumulator : currentValue}
+    
+    ,0)
+    
+
+
 
 };
 
